82dbce790cc90846bde3031ff5b7a67eac6a8a5b
eNJi25 <nfischer.contact@gmail.com>
2024-07-26 00:42:16 +0100

    Ajout de la page Réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,12 @@ export const allRoutes = [
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
   new Route("/account", "Mon Compte", "/pages/auth/account.html"),
-  new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+  new Route(
+    "/editPassword",
+    "Changement de mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
+  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
