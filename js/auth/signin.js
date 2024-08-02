4f0f5e06b8691baf8b1b279c9c341fa4e56717f9
eNJi25 <nfischer.contact@gmail.com>
2024-08-02 23:44:08 +0100

    Ajout des fichiers js signin.js en dur et script.js pour la gestion des cookies

---
+++ js/auth/signin.js
@@ -0,0 +1,20 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+  // Ici il faudra appeler l'API pour vérifiers les credentials en BDD
+  if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
+    // Il faudra ici récupérer le vrai token
+    const token = "jojiohepighpzgjzoehgopejgpizehogezpogjez"
+    setToken(token);
+    // Placer ce token en cookie
+
+    window.location.replace("/");
+  } else {
+    mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+  }
+}
