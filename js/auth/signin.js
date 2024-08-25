e8b80ef705c9b20457dd0aa62be983eb09dc5d68
eNJi25 <nfischer.contact@gmail.com>
2024-08-25 18:27:13 +0200

    Ajout fetch sur API studi sur connexion et inscription

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,20 +1,42 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials() {
-  // Ici il faudra appeler l'API pour vérifiers les credentials en BDD
-  if (mailInput.value == "test@mail.com" && passwordInput.value == "123") {
-    // Il faudra ici récupérer le vrai token
-    const token = "jojiohepighpzgjzoehgopejgpizehogezpogjez"
-    setToken(token);
-    // Placer ce token en cookie
-    setCookie(RoleCookieName, "admin", 7);
-    window.location.replace("/");
-  } else {
-    mailInput.classList.add("is-invalid");
-    passwordInput.classList.add("is-invalid");
-  }
+  let dataForm = new FormData(signinForm);
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let raw = JSON.stringify({
+    username: dataForm.get("email"),
+    password: dataForm.get("mdp"),
+  });
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  fetch(apiUrl + "login", requestOptions)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+      }
+    })
+    .then((result) => {
+      const token = result.apiToken;
+      setToken(token);
+      // Placer ce token en cookie
+      setCookie(RoleCookieName, result.roles[0], 7);
+      window.location.replace("/");
+    })
+    .catch((error) => console.log("error", error));
 }
--- js/auth/signup.js
+++ js/auth/signup.js
@@ -4,12 +4,14 @@ const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidatePassword = document.getElementById("ValidatePasswordInput");
 const btnValidation = document.getElementById("btn-validation-inscription");
+const formInscription = document.getElementById("formulaireInscription");
 
 inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidatePassword.addEventListener("keyup", validateForm);
+btnValidation.addEventListener("click", inscrireUtilisateur);
 
 function validateForm() {
   const nomOk = validateRequired(inputNom);
@@ -79,4 +81,43 @@ function validateRequired(input) {
     input.classList.add("is-invalid");
     return false;
   }
+}
+
+function inscrireUtilisateur() {
+  let dataForm = new FormData(formInscription);
+
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let raw = JSON.stringify({
+    firstName: dataForm.get("nom"),
+    lastName: dataForm.get("prenom"),
+    email: dataForm.get("email"),
+    password: dataForm.get("mdp"),
+  });
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  fetch(apiUrl + "registration", requestOptions)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("Erreur lors de l'inscription");
+      }
+    })
+    .then((result) => {
+      alert(
+        "Bravo " +
+          dataForm.get("prenom") +
+          ", vous êtes maintenant inscrit, vous pouvez vous connecter."
+      );
+      document.location.href = "/signin";
+    })
+
+    .catch((error) => console.log("error", error));
 }
